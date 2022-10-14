d535b348d9c8002343e00ea325e003cb6b6a5073
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-10-14 15:29:32 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,48 @@
-fn main() {}
+fn main() {
+    let example = Node {
+        doc: Some("Example!".to_string()),
+        value: Value::AttrSet(vec![Attr {
+            ident: "hasAttrByPath".to_string(),
+            node: Node {
+                doc: Some(
+                    r#"Return if an attribute from nested attribute set exists.
+                    Example:
+
+                    ```
+                    let
+                      x = {
+                        a = {
+                          b = 3;
+                        };
+                      };
+                    in assert hasAttrByPath ["a" "b"] x; true
+                    ```"#
+                        .to_string(),
+                ),
+                value: Value::Function(Function),
+            },
+        }]),
+    };
+}
 
 struct Node {
-    doc: Option<&'static str>,
+    doc: Option<String>,
     value: Value,
 }
 
 struct Attr {
-    ident: &'static str,
+    ident: String,
     node: Node,
 }
 
 enum Value {
     AttrSet(Vec<Attr>),
     Number,
-    String(&'static str),
-    Function,
+    Str(String),
+    Function(Function),
     Bool,
 }
 
-use Value::*;
-
-const EXAMPLE: Node = Node {
-    doc: Some("Example!"),
-    value: AttrSet(vec![Attr {
-        ident: "hasAttrByPath",
-        node: Node {
-            doc: Some(
-                r#"Return if an attribute from nested attribute set exists.
-                Example:
-
-                ```
-                let
-                  x = {
-                    a = {
-                      b = 3;
-                    };
-                  };
-                in assert hasAttrByPath ["a" "b"] x; true
-                ```"#,
-            ),
-            value: todo!(),
-        },
-    }]),
-};
+struct Function {
+    param: 
+}
