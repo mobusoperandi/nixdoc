af5540818844cfaa535c65a5e78e1ed32b6f760b
a-kenji <aks.kenji@protonmail.com>
2022-10-14 09:29:09 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:.gitignore

--- examples/input.nix
+++ examples/input.nix
@@ -14,8 +14,11 @@
   hasAttrByPath = attrPath: e:
     let attr = head attrPath;
     in
-      if attrPath == [] then true
-      else if e ? ${attr}
-      then hasAttrByPath (tail attrPath) e.${attr}
-      else false;
+    if attrPath == [ ] then true
+    else if e ? ${attr}
+    then hasAttrByPath (tail attrPath) e.${attr}
+    else false;
+  ## Returns true if a provided number is greater than 3.
+  isGreaterThan3 = n: n > 3;
+  system = "x86_64-linux";
 }
